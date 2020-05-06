f1504cdf5e24b09aa8f91197a6ad4a229dd257be
Utkarsh Jaiswal <utkjaiswal58@gmail.com>
2020-05-07 01:44:37 +0530

    Changes in content1

--- js/main.js
+++ js/main.js
@@ -115,3 +115,13 @@
 	});
 
 })(jQuery);
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+
+
